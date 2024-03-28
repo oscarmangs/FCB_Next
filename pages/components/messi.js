dd4168e4956bd92b951203f959e7fccd25d3db16
oscarmangs <oscar.maangs@gmail.com>
2024-03-28 10:32:43 +0100

    added GTM and exported topNav to a layout

--- pages/components/messi.js
+++ pages/components/messi.js
@@ -1,11 +1,19 @@
 import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import classes from "../components/messi.module.css";
+import { shouldShowMessiComponent} from "../../optimizely-js-quickstart";
 
 export default function Messi() {
+  const [showMessi, setShowMessi] = useState(false);
+  
+
+
+  useEffect(() => {
+    setShowMessi(shouldShowMessiComponent("user_id_here"));
+  }, []);
 
   const messiRef = useRef(null);
-    const [isMirrored, setIsMirrored] = useState(false);
+  const [isMirrored, setIsMirrored] = useState(false);
   const keysPressed = {};
 
   useEffect(() => {
@@ -26,28 +34,36 @@ export default function Messi() {
       document.removeEventListener("keydown", handleKeyDown);
       document.removeEventListener("keyup", handleKeyUp);
     };
-  }, []);
+  });
 
   const handleKeys = () => {
-    const currentTop = parseInt(messiRef.current.style.top) || 300;
-    const currentLeft = parseInt(messiRef.current.style.left) || 40;
+    // Check if messiRef and messiRef.current are not null
+    if (messiRef && messiRef.current) {
+      const currentTop = parseInt(messiRef.current.style.top) || 300;
+      const currentLeft = parseInt(messiRef.current.style.left) || 40;
 
-    if (keysPressed["ArrowDown"]) {
-      messiRef.current.style.top = currentTop + 10 + "px";
-    }
+      if (keysPressed["ArrowDown"]) {
+        messiRef.current.style.top = currentTop + 10 + "px";
+      }
 
-    if (keysPressed["ArrowUp"]) {
-      messiRef.current.style.top = currentTop - 10 + "px";
-    }
+      if (keysPressed["ArrowUp"]) {
+        messiRef.current.style.top = currentTop - 10 + "px";
+      }
 
-    if (keysPressed["ArrowRight"]) {
-      setIsMirrored(false);
-      messiRef.current.style.left = currentLeft + 10 + "px";
-    } else if (keysPressed["ArrowLeft"]) {
-      setIsMirrored(true);
-      messiRef.current.style.left = currentLeft - 10 + "px";
+      if (keysPressed["ArrowRight"]) {
+        setIsMirrored(false);
+        messiRef.current.style.left = currentLeft + 10 + "px";
+      } else if (keysPressed["ArrowLeft"]) {
+        setIsMirrored(true);
+        messiRef.current.style.left = currentLeft - 10 + "px";
+      }
     }
   };
+
+
+  if (!showMessi) {
+    return null; // Don't render Messi component on the server side
+  }
 
   return (
     <Image
